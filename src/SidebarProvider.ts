ab2511327180f98449e48c9e4c9ebd225e091c0d
jaykingamez <jaykinchan@gmail.com>
2022-11-01 10:56:35 +0800

    feat: Add sidebar

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -2,66 +2,73 @@ import * as vscode from "vscode";
 import { getNonce } from "./getNonce";
 
 export class SidebarProvider implements vscode.WebviewViewProvider {
-  _view?: vscode.WebviewView;
-  _doc?: vscode.TextDocument;
+    _view?: vscode.WebviewView;
+    _doc?: vscode.TextDocument;
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+    constructor(private readonly _extensionUri: vscode.Uri) {}
 
-  public resolveWebviewView(webviewView: vscode.WebviewView) {
-    this._view = webviewView;
+    public resolveWebviewView(webviewView: vscode.WebviewView) {
+        this._view = webviewView;
 
-    webviewView.webview.options = {
-      // Allow scripts in the webview
-      enableScripts: true,
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
 
-      localResourceRoots: [this._extensionUri],
-    };
+            localResourceRoots: [this._extensionUri],
+        };
 
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-    webviewView.webview.onDidReceiveMessage(async (data) => {
-      switch (data.type) {
-        
-        case "onInfo": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showInformationMessage(data.value);
-          break;
-        }
-        case "onError": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showErrorMessage(data.value);
-          break;
-        }
-      }
-    });
-  }
+        webviewView.webview.onDidReceiveMessage(async (data) => {
+            switch (data.type) {
+                case "onInfo": {
+                    if (!data.value) {
+                        return;
+                    }
+                    vscode.window.showInformationMessage(data.value);
+                    break;
+                }
+                case "onError": {
+                    if (!data.value) {
+                        return;
+                    }
+                    vscode.window.showErrorMessage(data.value);
+                    break;
+                }
+            }
+        });
+    }
 
-  public revive(panel: vscode.WebviewView) {
-    this._view = panel;
-  }
+    public revive(panel: vscode.WebviewView) {
+        this._view = panel;
+    }
 
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-    );
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
-    );
-    const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
-    );
-    const styleVSCodeUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-    );
+    private _getHtmlForWebview(webview: vscode.Webview) {
+        const styleResetUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+        );
+        const scriptUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(
+                this._extensionUri,
+                "out",
+                "compiled/sidebar.js"
+            )
+        );
+        const styleMainUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(
+                this._extensionUri,
+                "out",
+                "compiled/sidebar.css"
+            )
+        );
+        const styleVSCodeUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+        );
 
-    // Use a nonce to only allow a specific script to be run.
-    const nonce = getNonce();
+        // Use a nonce to only allow a specific script to be run.
+        const nonce = getNonce();
 
-    return `<!DOCTYPE html>
+        return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -69,17 +76,25 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 					Use a content security policy to only allow loading images from https or from our extension directory,
 					and only allow scripts that have a specific nonce.
         -->
-        <meta http-equiv="Content-Security-Policy" img-src https: data:; style-src 'unsafe-inline' ${
-      webview.cspSource
-    }; script-src 'nonce-${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-        <link href="${styleMainUri}" rel="stylesheet">
+        <meta http-equiv="Content-Security-Policy" img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 			</head>
-      <body>
-				<script nonce="${nonce}" src="${scriptUri}"></script>
+            <body>
+            <h1>Dear Princess Celestia</h1>
+            <u1>
+                <li><a href="https://owasp.org/www-project-dependency-check/">OWASP</a></li>
+                <li><a href="https://owasp.org/www-community/attacks/xss/">XSS</a></li>
+                <li><a href="https://owasp.org/www-community/attacks/SQL_Injection">SQLi</a></li>
+                <li><a href="https://owasp.org/www-community/attacks/Command_Injection">Command Injection</a></li>
+                <li><a href="https://cheatsheetseries.owasp.org/cheatsheets/Insecure_Direct_Object_Reference_Prevention_Cheat_Sheet.html">IDOR</a></li>
+                
+                
+                
+                
+                
+            </u1>
+
 			</body>
 			</html>`;
-  }
+    }
 }
